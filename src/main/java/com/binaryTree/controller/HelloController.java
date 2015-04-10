3c2b8993e2ea511c84e4ae23c20c3c4ca023d5a7
rafael <rcarrasco@nisum.com>
2015-04-10 09:25:51 -0300

    first commit

---
+++ src/main/java/com/binaryTree/controller/HelloController.java
@@ -0,0 +1,53 @@
+package com.binaryTree.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = "/",method = RequestMethod.GET)
+    @ResponseBody
+    public int printWelcome() {
+        
+        List<Integer> numbers = generateData();
+
+        for (Integer number : numbers) {
+
+            System.out.println(number.intValue());
+            //return number.intValue();
+        }
+
+        return 999;
+	}
+    
+    public List<Integer> generateData(){
+        
+        List<Integer> numbers = new ArrayList<Integer>();
+
+        Random ran = new Random();
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        for(int i = 1; i <= 14; i++)
+            al.add(i);
+        int[] number = new int[9];
+
+        for(int i = 0; i < number.length; i++)
+        {
+            number[i] = al.remove(ran.nextInt(al.size()));
+
+            numbers.add(number[i]);
+        }
+
+
+        Collections.shuffle(numbers);
+        return numbers;
+    }
+}
---
+++ src/main/java/com/binaryTree/domain/Node.java
@@ -0,0 +1,40 @@
+package com.binaryTree.domain;
+
+
+public class Node {
+
+    private int key;
+    private Node leftNode;
+    private Node rightNode;
+
+    public Node() {
+    }
+
+    public Node(int key) {
+        this.key = key;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public Node getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(Node leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public Node getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(Node rightNode) {
+        this.rightNode = rightNode;
+    }
+}
